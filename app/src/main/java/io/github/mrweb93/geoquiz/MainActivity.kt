c195e3c0c5192959bad68116b4684cac03a270df
Eugene <mr_w_e_b@mail.ru>
2021-08-06 21:20:21 +1100

    Initial commit

---
+++ app/src/main/java/io/github/mrweb93/geoquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.mrweb93.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
