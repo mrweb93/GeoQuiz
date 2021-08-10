92a6f76c4d97ad62350987b4b584b55adde57a24
Eugene <mr_w_e_b@mail.ru>
2021-08-11 08:30:58 +1100

    Глава 3 Упражнение

--- app/src/main/java/io/github/mrweb93/geoquiz/MainActivity.kt
+++ app/src/main/java/io/github/mrweb93/geoquiz/MainActivity.kt
@@ -2,7 +2,9 @@ package io.github.mrweb93.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,9 +12,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
+    private val TAG="MainActivity"
+
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -24,9 +28,12 @@ class MainActivity : AppCompatActivity() {
     )
 
     private var currentIndex = 0
+    private var trueAnswer =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -45,16 +52,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
+            if (currentIndex==questionBank.size) Toast.makeText(this, ((trueAnswer/questionBank.size)*100).toString(), Toast.LENGTH_LONG).show()
             currentIndex = (currentIndex+1) % questionBank.size
             updateQuestion()
         }
 
         prevButton.setOnClickListener {
-            if (currentIndex == 0) currentIndex = questionBank.size-1 else currentIndex = currentIndex-1
+            if (currentIndex == 0) currentIndex = 0 else currentIndex = currentIndex-1
             updateQuestion()
         }
 
         questionTextView.setOnClickListener {
+
+            if (currentIndex==questionBank.size-1)
+                Toast.makeText(this, ((trueAnswer/questionBank.size)*100).toString(), Toast.LENGTH_LONG).show()
+
             currentIndex = (currentIndex+1) % questionBank.size
             updateQuestion()
         }
@@ -62,19 +74,57 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+
+        trueButton.isEnabled=true
+        falseButton.isEnabled=true
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
+
+        var messageResId=R.string.incorrect_toast
+
+        if (userAnswer == correctAnswer) {
+            messageResId= R.string.correct_toast
+            trueAnswer++
         } else
         {
-            R.string.incorrect_toast
+            trueAnswer--
         }
+
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        trueButton.isEnabled=false
+        falseButton.isEnabled=false
+
     }
 }
